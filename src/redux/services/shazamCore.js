3173fb9d0b9ffd74e2960bfb484079c1c08d2753
KingLord147 <rajprabhu.147@gmail.com>
2025-11-13 19:51:20 +0530

    Api Recongigure

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,24 +1,41 @@
-/* eslint-disable no-tabs */
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const shazamCoreApi = createApi({
-  reducerPath: 'shazamCoreApi',
+  reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: "https://shazam-core.p.rapidapi.com",
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', 'a7c75bc1e8msh86463afd67bad11p174531jsnbb0ff844c5b1');
-
+      headers.set(
+        "X-RapidAPI-Key",
+        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+      );
+      headers.set("X-RapidAPI-Host", "shazam-core.p.rapidapi.com");
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
-    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-    getArtistDetails: builder.query({ query: ({ artistId }) => `/artists/details?artist_id=${artistId}` }),
-    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
-    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getTopCharts: builder.query({
+      query: () => "/v2/charts/track",
+    }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/v2/charts/genre?genre_code=${genre}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/v2/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/v2/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/v2/artists/details?artist_id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/v2/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/v2/search?q=${encodeURIComponent(searchTerm)}&limit=10`,
+    }),
   }),
 });
 
